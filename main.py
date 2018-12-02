ced6ca31413fcea8be941212266d47b3f4403825
Sergey Minchuk <minchuk94@gmail.com>
2018-12-02 19:41:48 +0300

    Add compression algorithm

--- main.py
+++ main.py
@@ -1,66 +1,98 @@
 import cv2
 import numpy as np
 import math
+from pathlib import Path
 
 
 def main():
-    # sample = np.arange(100 * 3).reshape(10, 10, 3)
-    # sample = block_shaped(sample, 4, 4)
-    # p, t, y, u = sample.shape
-    image = cv2.imread('source_image.jpg')
-    image_height, image_width, S = image.shape
-    m = 10
-    n = 10
+    image_name = Path('images') / '1lina32x32.jpg'
+    P = [30, 40, 50]
+    e = 5
+    m = 5
+    n = 5
+    N = m * n * 3
 
-    np.random.seed(1)
-    transformed_pixels = 2 * image[:, :] / np.max(image) - 1
-    pixels = block_shaped(transformed_pixels, m, n)
-    pixel_blocks = np.split(pixels, pixels.shape[0], axis=0)
-    pixel_blocks_shape = pixel_blocks[0].shape
-    pixel_blocks_X = []
-    for pb in pixel_blocks:
-        pixel_blocks_X.append(pb.reshape(-1))
-    N = m * n * S
-    p = 20
-    e = 0.005 * m * n
-    e_ = e / p
+    h, w, s, block_shape, source_shape, pixels = prepare_image(image_name, m, n)
+    for p in P:
+        E, t, Z, L, W, W_ = train(pixels, N, p, e, image_name)
+        restore_image(W, W_, pixels, block_shape, source_shape, h, w, m, n, image_name, N, L, E, p)
+        print('=====================')
+
+
+def prepare_image(image_name, m, n):
+    image = cv2.imread(str(image_name))
+    h, w, s = image.shape
+    image = image.astype('float32')
+    transformed_pixels = 2 * image / 255 - 1
+    source_shape, pixels = block_shaped(transformed_pixels, m, n)
+    block_shape = pixels[0].shape
+    pixels = pixels.reshape(pixels.shape[0], -1)
+    return h, w, s, block_shape, source_shape, pixels
+
+
+def train(pixels, N, p, e, image_name):
+    W = np.random.uniform(-1, 1, N * p).reshape(N, p)
+    W_ = W.copy().transpose()
     E = math.inf
     E_ = math.inf
-    W = np.random.uniform(-1, 1, N * p).reshape(N, p)
-    W_ = W.transpose()
-    Eq = np.empty(len(pixel_blocks_X))
-    Eq_ = np.empty(len(pixel_blocks_X))
+    L = len(pixels)
+    Eq = np.empty(L)
+    Eq_ = np.empty(L)
     t = 0
 
-    while E > e or E_ > e_:
+    while E > e or E_ > e:
         t += 1
-        for i, X in enumerate(pixel_blocks_X):
-            Y = np.apply_along_axis(lambda col: np.sum(col * X), axis=0, arr=W)
-            X_ = np.apply_along_axis(lambda col: np.sum(col * Y), axis=0, arr=W_)
-            Y_ = np.apply_along_axis(lambda col: np.sum(col * X_), axis=0, arr=W)
+        for i, X in enumerate(pixels):
+            Y = W.T @ X
+            X_ = W_.T @ Y
+            Y_ = W.T @ X_
 
-            alpha = 1 / np.sum(X_ * X_)
-            alpha_ = 1 / np.sum(Y * Y)
+            alpha = 1 / math.pow(np.sum(X_ * X_), 2)
+            alpha_ = 1 / math.pow(np.sum(Y * Y), 2)
 
-            W = W - alpha * X_[:, np.newaxis] @ (Y_ - Y)[np.newaxis, :]
-            W_ = W_ - alpha_ * Y[:, np.newaxis] @ (X_ - X)[np.newaxis, :]
+            W = W - alpha * (X_[:, np.newaxis] @ (Y_ - Y)[np.newaxis, :])
+            W_ = W_ - alpha_ * (Y[:, np.newaxis] @ (X_ - X)[np.newaxis, :])
 
-            len_W = np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=1, arr=W)
-            len_W_ = np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=1, arr=W_)
-
-            W = W / len_W[:, np.newaxis]
-            W_ = W_ / len_W_[:, np.newaxis]
+            W = W / np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=0, arr=W)
+            W_ = W_ / np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=0, arr=W_)
 
             dX = X_ - X
             Eq[i] = np.sum(dX * dX, axis=0) / 2
 
             dY = Y_ - Y
             Eq_[i] = np.sum(dY * dY, axis=0) / 2
-        E = np.sum(Eq, axis=0) / len(pixel_blocks_X)
-        E_ = np.sum(Eq_, axis=0) / len(pixel_blocks_X)
-        print("Error: {}. Error': {}".format(E, E_))
-    np.save('model1', W)
-    np.save('model_1', W)
+
+        E = np.sum(Eq, axis=0)
+        E_ = np.sum(Eq_, axis=0)
+
+    print('Image={}'.format(image_name))
+    print('Iterations={}'.format(t))
+    print('E={}'.format(E))
+    Z = (N * L) / ((N + L) * p + 2)
+    print('Z={}'.format(Z))
+    model_folder = Path('models')
+    readable_model_folder = model_folder / 'readable'
+    name_model = 'IMAGE{}_N{}_L{}_E{}_P{}model'.format(image_name, N, L, E, p)
+    name_model_ = 'IMAGE{}_N{}_L{}_E{}_P{}model_'.format(image_name, N, L, E, p)
+    np.save(model_folder / name_model, W)
+    np.save(model_folder / name_model_, W)
+    with open(readable_model_folder / name_model, 'wt') as f:
+        print('W', file=f)
+        print([col for col in W], file=f)
+    with open(readable_model_folder / name_model, 'wt') as f:
+        print("W'", file=f)
+        print([col for col in W_], file=f)
+    return E, t, Z, L, W, W_
+
+
+def restore_image(W, W_, pixels, block_shape, source_shape, h, w, m, n, image_name, N, L, E, p):
+    mm_W_W_ = W_.T @ W.T
+    Y = np.array([mm_W_W_ @ X for X in pixels])
+    restored_pixels = Y.reshape(pixels.shape[0], block_shape[0], block_shape[1], block_shape[2])
+    restored_merged_pixels = block_unshaped(restored_pixels, source_shape, h, w, m, n)
+    restored_merged_pixels = 255 * (restored_merged_pixels + 1) / 2
+    restored_merged_pixels = np.rint(restored_merged_pixels)
+    cv2.imwrite('restored_images/IMAGE{}_N{}_L{}_E{}_P{}restored.jpg'.format(image_name, N, L, E, p), restored_merged_pixels)
 
 
 def block_shaped(arr, m, n):
@@ -76,23 +108,22 @@ def block_shaped(arr, m, n):
     arr = np.vstack((arr[:pivot_r, :], arr[start_r:pivot_r, :], arr[pivot_r:, :]))
     h, w, d = arr.shape
 
-    return arr.reshape(h // m, m, -1, n, d).swapaxes(1, 2).reshape(-1, m, n, d)
+    return arr.shape, arr.reshape(h // m, m, -1, n, d).swapaxes(1, 2).reshape(-1, m, n, d)
 
 
-def block_shaped_test(arr, m, n):
-    h = arr.shape[0]
-    w = arr.shape[1]
+def block_unshaped(arr, source_shape, h, w, m, n):
+    arr = arr.reshape(source_shape[0] // m, source_shape[1] // m, m, n, 3).swapaxes(1, 2).reshape(source_shape)
+
     pivot_c = w - w % n
     start_c = w - n
 
     pivot_r = h - h % m
     start_r = h - m
 
-    arr = np.hstack((arr[:, :pivot_c], arr[:, start_c:pivot_c], arr[:, pivot_c:]))
-    arr = np.vstack((arr[:pivot_r, :], arr[start_r:pivot_r, :], arr[pivot_r:, :]))
-    h, w = arr.shape
+    arr = np.hstack((arr[:, :pivot_c], arr[:, pivot_c + pivot_c - start_c:]))
+    arr = np.vstack((arr[:pivot_r, :], arr[pivot_r + pivot_r - start_r:, :]))
 
-    return arr.reshape(h // m, m, -1, n).swapaxes(1, 2).reshape(-1, m, n)
+    return arr
 
 
 if __name__ == '__main__':
--- puk.py
+++ puk.py
@@ -10,10 +10,10 @@ def main():
     # p, t, y, u = sample.shape
     image = cv2.imread('source_image.jpg')
     image_height, image_width, S = image.shape
-    m = 20
-    n = 20
-
-    transformed_pixels = 2 * image[:, :] / np.max(image) - 1
+    m = 15
+    n = 15
+    image = image.astype('float32')
+    transformed_pixels = 2 * image / np.max(image) - 1
     pixels = block_shaped(transformed_pixels, m, n)
     pixel_blocks = np.split(pixels, pixels.shape[0], axis=0)
     pixel_blocks_shape = pixel_blocks[0].shape
@@ -23,20 +23,18 @@ def main():
 
     N = m * n * S
     p = 20
-    e = 0.005 * m * n * p
-    e_ = e / p
+    e = 0.0001 * m * n * S * p
+    e_ = e
     E = math.inf
     E_ = math.inf
-    W = np.random.uniform(-1, 1, N * p).reshape(N, p)
+    W = np.random.uniform(-1, 1, N * N).reshape(N, N)
     W_ = W.transpose()
-    Wn = W
-    W_n = W_
-    dXs = np.empty((len(pixel_blocks_X), W.shape[0]))
     Eq = np.empty(len(pixel_blocks_X))
     Eq_ = np.empty(len(pixel_blocks_X))
     t = 0
-    # alpha = 0.000001 * m * n * p
-    # alpha_ = 0.001
+    alpha = 0.00001
+    alpha_ = 0.00001
+    Emin = math.inf
 
     while E > e or E_ > e_:
         t += 1
@@ -45,31 +43,31 @@ def main():
             X_ = np.apply_along_axis(lambda col: np.sum(col * Y), axis=0, arr=W_)
             Y_ = np.apply_along_axis(lambda col: np.sum(col * X_), axis=0, arr=W)
 
-            alpha = 1 / np.sum(X_ * X_)
-            alpha_ = 1 / np.sum(Y * Y)
+            alpha = 1 / math.pow(np.sum(X_ * X_), 2)
+            alpha_ = 1 / math.pow(np.sum(Y * Y), 2)
 
-            W = W - alpha * X_[:, np.newaxis] @ (Y_ - Y)[np.newaxis, :]
-            W_ = W_ - alpha_ * Y[:, np.newaxis] @ (X_ - X)[np.newaxis, :]
+            W = W - alpha * (X_[:, np.newaxis] @ (Y_ - Y)[np.newaxis, :])
+            W_ = W_ - alpha_ * (Y[:, np.newaxis] @ (X_ - X)[np.newaxis, :])
 
-            len_W = np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=1, arr=W)
-            len_W_ = np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=0, arr=W_)
-
-            W = W / len_W[:, np.newaxis]
-            W_ = W_ / len_W_
+            W = W / np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=0, arr=W)
+            W_ = W_ / np.apply_along_axis(lambda col: math.sqrt(np.sum(col * col)), axis=0, arr=W_)
 
             dX = X_ - X
             Eq[i] = np.sum(dX * dX, axis=0) / 2
 
             dY = Y_ - Y
             Eq_[i] = np.sum(dY * dY, axis=0) / 2
-            # print("Error: {}. Error': {}".format(Eq[i], Eq_[i]))
-            # time.sleep(0.5)
+
+            if Eq[i] < Emin:
+                Emin = Eq[i]
+            print("Error: {}. Error': {}. Min: {}".format(Eq[i], Eq_[i], Emin))
+            # time.sleep(0.1)
             # print("Error: {}. Error': {}".format(E, E_))
             # print(Eq[i])
             # print(i)
         E = np.sum(Eq, axis=0) / len(pixel_blocks_X)
         E_ = np.sum(Eq_, axis=0) / len(pixel_blocks_X)
-        print("Error: {}. Error': {}".format(E, E_))
+        print("SUM=====Error: {}. Error': {}".format(E, E_))
 
 
 def block_shaped(arr, m, n):
